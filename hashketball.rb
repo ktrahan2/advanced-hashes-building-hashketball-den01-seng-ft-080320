16e5d44b9c99a2937dba84aeb6ae4c4f4974e9de
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-24 18:58:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -61,13 +61,65 @@ def game_hash
           blocks: 11
           slam_dunks: 1
         }
+        ],
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"],
+      players: = [
+        { 
+          player_name: "Alan Anderson"
+          number: 0
+          shoe: 16
+          points: 22
+          rebounds: 12
+          assits: 12
+          steals: 3
+          blocks: 1
+          slam_dunks: 1
+        }, 
+        { 
+          player_name: "Reggie Evans"
+          number: 0
+          shoe: 14
+          points: 12
+          rebounds: 12
+          assits: 12
+          steals: 12
+          blocks: 12
+          slam_dunks: 7
+        },
+        {
+          player_name: "Brook Lopez"
+          number: 11
+          shoe: 17
+          points: 17
+          rebounds: 19
+          assits: 10
+          steals: 3
+          blocks: 1
+          slam_dunks: 15
+        }, 
+        {
+          player_name: "Mason Plumlee"
+          number: 1
+          shoe: 19
+          points: 26
+          rebounds: 11
+          assits: 6
+          steals: 3
+          blocks: 8
+          slam_dunks: 5
+        },
+        {
+          player_name: "Jason Terry"
+          number: 31
+          shoe: 15
+          points: 19
+          rebounds: 2
+          assits: 2
+          steals: 4
+          blocks: 11
+          slam_dunks: 1
+        }
         ]
-    :away => {
-      team_name: => "",
-      colors: => ["", ""],
-      players: => [
-       {}
-        ]
-      }
-  }
 end
