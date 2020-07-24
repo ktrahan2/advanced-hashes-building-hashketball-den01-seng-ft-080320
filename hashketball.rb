e4d52bb493f97cd0fed1d5042fafb63a5be1b8d2
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-24 18:53:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -18,6 +18,17 @@ def game_hash
           slam_dunks: => 1
         }, 
         { 
+          player_name: => "Reggie Evans"
+          number: => 30
+          shoe: => 
+          points: => 22
+          rebounds: => 12
+          assits: => 12
+          steals: => 3
+          blocks: => 1
+          slam_dunks: => 1
+        },
+        {
           player_name: => "Alan Anderson"
           number: => 0
           shoe: => 16
@@ -27,9 +38,6 @@ def game_hash
           steals: => 3
           blocks: => 1
           slam_dunks: => 1
-        },
-        {
-          
         } 
         ]
           
