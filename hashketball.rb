cdaba447711cb313c21048d47b6eef0b966ef978
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-24 19:04:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
     home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
-      players: = [
+      players: [
         { 
           player_name: "Alan Anderson"
           number: 0
@@ -65,7 +65,7 @@ def game_hash
     away: {
       team_name: "Charlotte Hornets",
       colors: ["Turqoise", "Purple"],
-      players: = [
+      players: [
         { 
           player_name: "Jeff Adrien"
           number: 4
