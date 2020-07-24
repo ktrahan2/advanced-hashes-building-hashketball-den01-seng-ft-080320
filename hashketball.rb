80185ceec19123d341d7dd77cd10334d2c0a82f2
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-24 18:54:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,14 +8,14 @@ def game_hash
       players: = [
         { 
           player_name: => "Alan Anderson"
-          number: => 0
-          shoe: => 16
-          points: => 22
-          rebounds: => 12
-          assits: => 12
-          steals: => 3
-          blocks: => 1
-          slam_dunks: => 1
+          number: 0
+          shoe: 16
+          points: 22
+          rebounds: 12
+          assits: 12
+          steals: 3
+          blocks: 1
+          slam_dunks: 1
         }, 
         { 
           player_name: => "Reggie Evans"
